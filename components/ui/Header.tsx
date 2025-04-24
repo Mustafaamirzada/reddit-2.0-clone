0e49da880e73a06897aebe7f03e9bb6dd6c26ca7
Mustafaamirzada <142759706+Mustafaamirzada@users.noreply.github.com>
2025-04-24 09:50:40 +0430

    creating sidebar

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,14 +1,41 @@
 "use client"
 
 import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/nextjs'
-import React from 'react'
+import { ChevronLeftIcon, MenuIcon } from "lucide-react"
 import { Button } from './button'
+import Image from 'next/image'
+import logo from '@/images/Reddit-Logo.png'
+import reddit from '@/images/reddit.png'
+import { useSidebar } from './sidebar'
 
 const Header = () => {
   const { user } = useUser();
+  const { toggleSidebar, open, isMobile } = useSidebar();
   return (
-    <header>Header
+    <header className='flex items-center justify-between p-2 border-b border-gray-200'>
       {/* left side */}
+      <div className='h-10 flex items-center'>
+        {open ? (
+          <ChevronLeftIcon className='w-6 h-6 cursor-pointer' onClick={toggleSidebar} />
+        ) : (
+          <div className="flex items-center gap-2">
+            <MenuIcon className='w-6 h-6 cursor-pointer' onClick={toggleSidebar} />
+            <Image src={logo}
+              alt='logo'
+              width={150}
+              height={150}
+              className='hidden md:block' />
+
+            <Image
+              src={reddit}
+              alt='reddit'
+              width={40}
+              height={40}
+              className='block md:hidden' />
+          </div>
+        )}
+
+      </div>
 
       {/* right side */}
       <div>
